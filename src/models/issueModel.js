27deade41f7af7d398704394e8ba6598b86c3e91
YahyaHafidz <yahya.hafidz@amarbank.co.id>
2025-07-19 16:39:15 +0700

    create issue model to handle query include create issue, findbyId issue, findAllIssue by project, update issue, delete issue

--- src/models/issueModel.js
+++ src/models/issueModel.js
@@ -1,1 +1,25 @@
-s
+const db = require('../config/db');
+
+const Issue = {
+  async create(issue) {
+    return db('issues').insert(issue).returning('*');
+  },
+
+  async findById(id) {
+    return db('issues').where({ id }).first();
+  },
+
+  async findAllByProject(projectId) {
+    return db('issues').where({ project_id: projectId });
+  },
+
+  async update(id, data) {
+    return db('issues').where({ id }).update(data).returning('*');
+  },
+
+  async delete(id) {
+    return db('issues').where({ id }).del();
+  }
+};
+
+module.exports = Issue;
