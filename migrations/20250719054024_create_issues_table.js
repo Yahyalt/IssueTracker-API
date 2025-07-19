22f63627186385f6be7150408b69f77bc9b9c3fb
YahyaHafidz <yahya.hafidz@amarbank.co.id>
2025-07-19 13:11:36 +0700

    add issue table column

--- migrations/20250719054024_create_issues_table.js
+++ migrations/20250719054024_create_issues_table.js
@@ -3,7 +3,16 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-      
+      return knex.schema.createTable('issues', function(table) {
+    table.increments('id').primary();
+    table.string('title').notNullable();
+    table.text('description');
+    table.enu('status', ['open', 'in_progress', 'closed']).defaultTo('open');
+    table.enu('priority', ['low', 'medium', 'high']).defaultTo('medium');
+    table.integer('project_id').unsigned().references('id').inTable('projects').onDelete('CASCADE');
+    table.integer('assignee_id').unsigned().references('id').inTable('users').onDelete('SET NULL');
+    table.timestamps(true, true);
+  });
 };
 
 /**
@@ -11,5 +20,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+   return knex.schema.dropTable('issues');
 };
