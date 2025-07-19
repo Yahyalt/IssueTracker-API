fc2c9582346a95c66981f74bb844d666f811266f
YahyaHafidz <yahya.hafidz@amarbank.co.id>
2025-07-19 16:29:48 +0700

    register routes for issue and project CRUD

--- src/app.js
+++ src/app.js
@@ -12,4 +12,12 @@ app.use(express.json());
 
 app.use('/api', authRoutes);
 
+//for issue management
+const issueRoutes = require('./routes/issueRoutes');
+app.use('/api/issues', issueRoutes);
+
+//for project management
+const projectRoutes = require('./routes/projectRoutes');
+app.use('/api/projects', projectRoutes);
+
 module.exports = app;
