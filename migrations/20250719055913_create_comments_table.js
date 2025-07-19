cb93062077e558881fef38ee11c04db1d4647932
YahyaHafidz <yahya.hafidz@amarbank.co.id>
2025-07-19 13:12:07 +0700

    add comments table column

--- migrations/20250719055913_create_comments_table.js
+++ migrations/20250719055913_create_comments_table.js
@@ -3,7 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+return knex.schema.createTable('comments', function(table) {
+    table.increments('id').primary();
+    table.text('content').notNullable();
+    table.integer('issue_id').unsigned().references('id').inTable('issues').onDelete('CASCADE');
+    table.integer('author_id').unsigned().references('id').inTable('users').onDelete('SET NULL');
+    table.timestamps(true, true);
+  });
 };
 
 /**
@@ -11,5 +17,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+return knex.schema.dropTable('comments');
 };
