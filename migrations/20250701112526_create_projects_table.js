793b64803d268c61acc740bb290ef55ef2d057f1
YahyaHafidz <yahya.hafidz@amarbank.co.id>
2025-07-19 13:05:06 +0700

    add projects table column

--- migrations/20250701112526_create_projects_table.js
+++ migrations/20250701112526_create_projects_table.js
@@ -3,7 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('projects', function(table) {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.text('description');
+    table.integer('created_by').unsigned().references('id').inTable('users').onDelete('SET NULL');
+    table.timestamps(true, true);
+  });
 };
 
 /**
@@ -11,5 +17,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('projects');
 };
